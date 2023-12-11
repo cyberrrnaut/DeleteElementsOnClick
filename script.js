f314c31d41927c54ed688ab52b4b651162032579
Soumyendu <cyberrrnaut@gmail.com>
2023-12-11 17:46:31 +0530

    final commit

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
- // document.getElementById('owl').onclick = function(){
-    //     alert("owl clicked")
-    // }
+// document.getElementById('owl').onclick = function(){
+//     alert("owl clicked")
+// }
 
-     // attachEvent()
-    // jQuery - on
+// attachEvent()
+// jQuery - on
 
-    // type, timestamp, defaultPrevented
-    // target, toElement, srcElement, currentTarget,
-    // clientX, clientY, screenX, screenY
-    // altkey, ctrlkey, shiftkey, keyCode
+// type, timestamp, defaultPrevented
+// target, toElement, srcElement, currentTarget,
+// clientX, clientY, screenX, screenY
+// altkey, ctrlkey, shiftkey, keyCode
 
-    // document.getElementById('images').addEventListener('click', function(e){
-    //     console.log("clicked inside the ul");
-    // }, false)
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log("clicked inside the ul");
+// }, false)
 
-    // document.getElementById('owl').addEventListener('click', function(e){
-    //     console.log("owl clicked");
-    //     e.stopPropagation()
-    // }, false)
-    
-    // document.getElementById('google').addEventListener('click',function(e){
-    //     e.preventDefault();
-    //     e.stopPropagation()
-    //     console.log("google clicked");
-    // }, false)
-   
-    
-    // document.querySelector('#images').addEventListener('click', function(e){
-    //     console.log(e.target.tagName);
-    //     if (e.target.tagName === 'IMG') {
-    //         console.log(e.target.id);
-    //         let removeIt = e.target.parentNode
-    //         removeIt.remove()
-    //     }
-    
-        
-    // })
-    
-   
+// document.getElementById('owl').addEventListener('click', function(e){
+//     console.log("owl clicked");
+//     e.stopPropagation()
+// }, false)
+
+// document.getElementById('google').addEventListener('click',function(e){
+//     e.preventDefault();
+//     e.stopPropagation()
+//     console.log("google clicked");
+// }, false)
+
+// document.querySelector('#images').addEventListener('click', function(e){
+//     console.log(e.target.tagName);
+//     if (e.target.tagName === 'IMG') {
+//         console.log(e.target.id);
+//         let removeIt = e.target.parentNode
+//         removeIt.remove()
+//     }
+
+// })
+
+const images = document.querySelector("#images").addEventListener('click',function(e){
+                 
+                  
+                     if (e.target.tagName === 'IMG'){ 
+
+                 let removeIT = e.target.parentNode;
+                     removeIT.remove();
+                     }
+                     
+},false);
 
 
-    //removeIt.parentNode.removeChild(removeIt)
+
+
+//removeIt.parentNode.removeChild(removeIt)
